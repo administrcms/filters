6e3a9d6c84c78d79e6f0e6af96e37d4197b54780
Miroslav Vitanov <mvvitanov@gmail.com>
2016-10-07 17:12:55 +0300

    Initial commit

---
+++ src/ListViewFiltersServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Administr\ListView\Filters;
+
+use Illuminate\Support\ServiceProvider;
+
+class ListViewFiltersServiceProvider extends ServiceProvider 
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
